75c237ab4fc3129cc8e6c5bc32d7070310316156
ali-f-alfa <ali.farahat78@gmail.com>
2021-11-18 19:32:34 +0330

    UI started

---
+++ SpaceWar/Assets/Scenes/EventSystemCustom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventSystemCustom : MonoBehaviour
+{
+    public UnityEvent OnCharacterHit;
+    public UnityEvent OnCharacterKillEnemy;
+    public UnityEvent OnGameEndedWon;
+    public UnityEvent OnGameEndedLost;
+
+    void Awake()
+    {
+        OnCharacterHit = new UnityEvent();
+        OnGameEndedWon = new UnityEvent();
+        OnGameEndedLost = new UnityEvent();
+    }
+}
---
+++ SpaceWar/Assets/Scenes/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text ScoreText;
+    public Text Life;
+    public EventSystemCustom eventSystem;
+    
+    void Start()
+    {
+        eventSystem.OnCharacterKillEnemy.AddListener(UpdateScore);
+        eventSystem.OnCharacterHit.AddListener(DecreaseLife);
+        //eventSystem.OnGameEndedWon.AddListener(EndGameWon);
+        //eventSystem.OnGameEndedLost.AddListener(EndGameLost);
+    }
+
+    public void UpdateScore()
+    {
+        int newTextValue = int.Parse(ScoreText.text) + 10;
+        ScoreText.text = newTextValue.ToString();
+    }
+
+    public void DecreaseLife()
+    {
+        int newTextValue = int.Parse(ScoreText.text) - 1;
+        Life.text = newTextValue.ToString();
+    }
+
+    //public void EndGameWon()
+    //{
+    //    if (int.Parse(KeyCounter.text) >= Constants.RequiredKeyCountToWin)
+    //    {
+    //        //Debug.Log("YOU WON!");
+    //        WinLabel.text = "You Won!";
+    //    }
+    //}
+
+    //public void EndGameLost()
+    //{
+    //    LostLabel.text = "You Lost!";
+    //}
+
+}
