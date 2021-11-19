bf62a270dc125b4e83a6ef127c52c28541358f4f
ali-f-alfa <ali.farahat78@gmail.com>
2021-11-19 13:14:24 +0330

    add enemy different missiles for each type

--- SpaceWar/Assets/Scenes/EnemyScript.cs
+++ SpaceWar/Assets/Scenes/EnemyScript.cs
@@ -62,28 +62,128 @@ public class EnemyScript : MonoBehaviour
             ReleaseHeart();
     }
 
-    public GameObject LunchMissile()
+    public void LunchMissile()
     {
-        GameObject go1 = new GameObject();
-        go1.name = "moving missile enemy";
+        if (TypeOfEnemy == EnemiesType.Type1)
+        {
+            GameObject go1 = new GameObject();
+            go1.name = "moving missile enemy";
 
-        Rigidbody2D missileRb = go1.AddComponent<Rigidbody2D>();
-        missileRb.bodyType = RigidbodyType2D.Kinematic;
+            Rigidbody2D missileRb = go1.AddComponent<Rigidbody2D>();
+            missileRb.bodyType = RigidbodyType2D.Kinematic;
 
-        go1.transform.position = this.transform.position - new Vector3(0, 0.2f, 0);
-        go1.transform.Rotate(new Vector3(180, 0, 0));
+            go1.transform.position = this.transform.position - new Vector3(0, 0.2f, 0);
+            go1.transform.Rotate(new Vector3(180, 0, 0));
 
-        SpriteRenderer spriteRenderer = go1.AddComponent<SpriteRenderer>();
-        spriteRenderer.sprite = MissileSprite;
+            SpriteRenderer spriteRenderer = go1.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = MissileSprite;
 
-        go1.tag = TagNames.EnemyMissile.ToString();
+            go1.tag = TagNames.EnemyMissile.ToString();
 
-        BoxCollider2D bc = go1.AddComponent<BoxCollider2D>() as BoxCollider2D;
-        bc.isTrigger = true;
+            BoxCollider2D bc = go1.AddComponent<BoxCollider2D>() as BoxCollider2D;
+            bc.isTrigger = true;
 
-        missileRb.velocity = transform.up * -1 * missileMovingSpeed;
+            missileRb.velocity = transform.up * -1 * missileMovingSpeed;
+        }
+        else if (TypeOfEnemy == EnemiesType.Type2)
+        {
+            GameObject go1 = new GameObject();
+            go1.name = "moving missile enemy";
 
-        return go1;
+            Rigidbody2D missileRb = go1.AddComponent<Rigidbody2D>();
+            missileRb.bodyType = RigidbodyType2D.Kinematic;
+
+            go1.transform.position = this.transform.position - new Vector3(-0.07f, 0.2f, 0);
+            go1.transform.Rotate(new Vector3(180, 0, 0));
+
+            SpriteRenderer spriteRenderer = go1.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = MissileSprite;
+
+            go1.tag = TagNames.EnemyMissile.ToString();
+
+            BoxCollider2D bc = go1.AddComponent<BoxCollider2D>() as BoxCollider2D;
+            bc.isTrigger = true;
+
+            missileRb.velocity = transform.up * -1 * missileMovingSpeed * 1.15f;
+            //second
+            GameObject go2 = new GameObject();
+            go2.name = "moving missile enemy";
+
+            Rigidbody2D missileRb2 = go2.AddComponent<Rigidbody2D>();
+            missileRb2.bodyType = RigidbodyType2D.Kinematic;
+
+            go2.transform.position = this.transform.position - new Vector3(0.07f, 0.2f, 0);
+            go2.transform.Rotate(new Vector3(180, 0, 0));
+
+            SpriteRenderer spriteRenderer2 = go2.AddComponent<SpriteRenderer>();
+            spriteRenderer2.sprite = MissileSprite;
+
+            go2.tag = TagNames.EnemyMissile.ToString();
+
+            BoxCollider2D bc2 = go2.AddComponent<BoxCollider2D>() as BoxCollider2D;
+            bc2.isTrigger = true;
+
+            missileRb2.velocity = transform.up * -1 * missileMovingSpeed * 1.15f;
+        }
+        else if (TypeOfEnemy == EnemiesType.Type3)
+        {
+            GameObject go1 = new GameObject();
+            go1.name = "moving missile enemy";
+
+            Rigidbody2D missileRb = go1.AddComponent<Rigidbody2D>();
+            missileRb.bodyType = RigidbodyType2D.Kinematic;
+
+            go1.transform.position = this.transform.position - new Vector3(-0.13f, 0.2f, 0);
+            go1.transform.Rotate(new Vector3(180, 0, 0));
+
+            SpriteRenderer spriteRenderer = go1.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = MissileSprite;
+
+            go1.tag = TagNames.EnemyMissile.ToString();
+
+            BoxCollider2D bc = go1.AddComponent<BoxCollider2D>() as BoxCollider2D;
+            bc.isTrigger = true;
+
+            missileRb.velocity = transform.up * -1 * missileMovingSpeed * 1.25f;
+            //
+            GameObject go2 = new GameObject();
+            go2.name = "moving missile enemy";
+
+            Rigidbody2D missileRb2 = go2.AddComponent<Rigidbody2D>();
+            missileRb2.bodyType = RigidbodyType2D.Kinematic;
+
+            go2.transform.position = this.transform.position - new Vector3(0, 0.2f, 0);
+            go2.transform.Rotate(new Vector3(180, 0, 0));
+
+            SpriteRenderer spriteRenderer2 = go2.AddComponent<SpriteRenderer>();
+            spriteRenderer2.sprite = MissileSprite;
+
+            go2.tag = TagNames.EnemyMissile.ToString();
+
+            BoxCollider2D bc2 = go2.AddComponent<BoxCollider2D>() as BoxCollider2D;
+            bc2.isTrigger = true;
+
+            missileRb2.velocity = transform.up * -1 * missileMovingSpeed * 1.25f;
+            //
+            GameObject go3 = new GameObject();
+            go3.name = "moving missile enemy";
+
+            Rigidbody2D missileRb3 = go3.AddComponent<Rigidbody2D>();
+            missileRb3.bodyType = RigidbodyType2D.Kinematic;
+
+            go3.transform.position = this.transform.position - new Vector3(0.13f, 0.2f, 0);
+            go3.transform.Rotate(new Vector3(180, 0, 0));
+
+            SpriteRenderer spriteRenderer3 = go3.AddComponent<SpriteRenderer>();
+            spriteRenderer3.sprite = MissileSprite;
+
+            go3.tag = TagNames.EnemyMissile.ToString();
+
+            BoxCollider2D bc3 = go3.AddComponent<BoxCollider2D>() as BoxCollider2D;
+            bc3.isTrigger = true;
+
+            missileRb3.velocity = transform.up * -1 * missileMovingSpeed *1.25f;
+        }
     }
 
     public GameObject ReleaseGift()
